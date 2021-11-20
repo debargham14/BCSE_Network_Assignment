c124311df8c0ecc6188cd6e0862cdac93c98746d
debargham14 <debargham14@gmail.com>
2021-11-21 01:32:02 +0530

    minor bug fixed in assignment7 DHCP protocol implementation

--- network_assignment7/DHCP/client.py
+++ network_assignment7/DHCP/client.py
@@ -5,13 +5,17 @@ import os
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 process_id = str(os.getpid())
 
+print ("Client started with process id :- " + process_id)
 count = 0
 while True:
        #ip ,port = input("Enter server ip address and port number :\n").split()
-       print (os.getpid())
-       m = input("Enter data to send server: ")
-       res = s.sendto(m.encode('utf-8'), ('127.0.0.1',12345))
-       data, addr = s.recvfrom(1024)
-       if count == 0: 
-           print (str(data.decode()))
+       if count == 1:
+           m = input ("Enter data to the server :")
+           res = s.sendto(m.encode('utf-8'), ('127.0.0.1',12345))
+       else:
+           res = s.sendto (process_id.encode('utf'), ('127.0.0.1', 12345))
+           data, addr = s.recvfrom(1024)
+           print ('Your assigned ip is :- ' + data.decode())
            count = 1
+       
+       
--- network_assignment7/DHCP/server.py
+++ network_assignment7/DHCP/server.py
@@ -7,12 +7,10 @@ import os
 s = socket.socket(socket.AF_INET , socket.SOCK_DGRAM )
 #binding IP and port 
 
-print (os.getpid())
 
 available_ip_queue = []
 available_ip_queue.append('0.0.0.0')
 available_ip_queue.append('0.0.0.1')
-available_ip_queue.append('0.0.0.2')
 
 connected_clients = []
 s.bind(('127.0.0.1',12345))
@@ -23,89 +21,51 @@ print("Waiting for Client response...")
 
 
 #dictionary to handle all the clients 
-clients = {}
+clients_pid_to_ip = {}
+clients_addr_to_pid = {}
 import errno
 import os
 
-def pid_exists(pid):
-    """Check whether pid exists in the current process table.
-    UNIX only.
-    """
-    if pid < 0:
-        return False
-    if pid == 0:
-        # According to "man 2 kill" PID 0 refers to every process
-        # in the process group of the calling process.
-        # On certain systems 0 is a valid PID but we have no way
-        # to know that in a portable fashion.
-        raise ValueError('invalid PID 0')
+
+def check_pid(pid):        
+    """ Check For the existence of a unix pid. """
     try:
         os.kill(pid, 0)
-    except OSError as err:
-        if err.errno == errno.ESRCH:
-            # ESRCH == No such process
-            return False
-        elif err.errno == errno.EPERM:
-            # EPERM clearly means there's a process to deny access to
-            return True
-        else:
-            # According to "man 2 kill" possible error values are
-            # (EINVAL, EPERM, ESRCH)
-            raise
+    except OSError:
+        return False
     else:
         return True
 
-def find_pid(inode):
 
-    # get a list of all files and directories in /proc
-    procFiles = os.listdir("/proc/")
-
-    # remove the pid of the current python process
-    procFiles.remove(str(os.getpid()))
-
-    # set up a list object to store valid pids
-    pids = []
-
-    for f in procFiles:
-        try:
-            # convert the filename to an integer and back, saving the result to a list
-            integer = int(f)
-            pids.append(str(integer))
-        except ValueError:
-            # if the filename doesn't convert to an integer, it's not a pid, and we don't care about it
-            pass
-    
-    for pid in pids:
-        # check the fd directory for socket information
-        fds = os.listdir("/proc/%s/fd/" % pid)
-        for fd in fds:
-            # save the pid for sockets matching our inode
-            if ('socket:[%d]' % inode) == os.readlink("/proc/%s/fd/%s" % (pid, fd)):
-                return pid
 while True:
-    data, addr = s.recvfrom(1024)
     dormant_clients = []
-    """
-    for key, value in clients.items():        
-        if pid_exists(key[1]) == False:
-            available_ip_queue.append(key[0])
-            dormant_clients.append(key[1])
-    print (dormant_clients)
-    for client in dormant_clients:
-        available_ip_queue.append(client)
-        clients.pop(client, None)
-    """
-    if addr in clients.keys():
-        s.sendto('acknowledgement msg'.encode(), addr)
-        print (str (data.decode()) + ' Received from client with ip :- ' + str(clients[addr]))
+    data, addr = s.recvfrom(1024)
+    
+    for key, value in clients_addr_to_pid.items():
+        if check_pid(value) == False:
+            dormant_clients.append(value)
+    #print (dormant_clients)
+    for client_pid in dormant_clients:
+        #print (str(client_pid) + ' freed')
+        available_ip_queue.append(clients_pid_to_ip[client_pid])
+        #del clients_pid_to_ip[client_pid]
+
+    
+    if addr in clients_addr_to_pid.keys():
+        print ( data.decode() + " sent to server by ip :- " + clients_pid_to_ip[clients_addr_to_pid[addr]])
     else:
-        msg = '' 
         if len(available_ip_queue) == 0: 
-            msg = 'Sorry client cannot be provided with ip :- '
+            msg = "Sorry ip address cannot be assigned !"
+            s.sendto(msg.encode('utf'), addr)
         else:
-            msg = 'hello new client your assigned ip is :- ' + available_ip_queue[0]
-            clients[addr] = available_ip_queue[0]
+            #print ("toubleshoot :- " + data.decode()) 
+            m = available_ip_queue[0]
+            client_pid = int(data.decode())
+            clients_addr_to_pid[addr] = client_pid
+            clients_pid_to_ip[client_pid] = available_ip_queue[0]
             available_ip_queue.pop(0)
-        s.sendto(msg.encode ('utf-8'), addr)       
+            res = s.sendto (m.encode('utf-8'), addr)
+
+
 
        
